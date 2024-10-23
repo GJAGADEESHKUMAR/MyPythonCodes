45c4c104ca3a563c6febaf57f216ed5a948f34cf
JagadeeshGaje <99236353+GJAGADEESHKUMAR@users.noreply.github.com>
2024-10-23 21:55:48 +0530

    strFormatting.py

---
+++ strFormatting.py
@@ -0,0 +1,28 @@
+#Python string formatting
+str = "{} {} {}".format('Believe', 'in', 'Your power')
+print(str)
+# Positional Formatting
+str = "{1} {0} {2}".format('Believe', 'in', 'Your Power')
+print(str)
+# Keyword Formatting
+str = "{l} {f} {g}".format(g='Believe', f='in', l='Your Power')
+print(str)
+
+# Formatting of Integers
+str1= "{0:b}".format(16)
+print("\nBinary representation of 16 is ")
+print(str1)
+str1="{0:o}".format(16)
+print("\nOctal representation of 16 is ")
+print(str1)
+
+# Formatting of Floats
+str1 = "{0:e}".format(16535.6458)
+print("\nExponent representation of 165.6458 is ")
+print(str1)
+
+# Rounding off Integers
+str1 = "{0:.4f}".format(5/9)
+print("\none-sixth is : ")
+print(str1)
+
