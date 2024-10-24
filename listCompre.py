ff6e682e52408240672b33a13166a16beed191a0
JagadeeshGaje <99236353+GJAGADEESHKUMAR@users.noreply.github.com>
2024-10-25 03:14:00 +0530

    listCompre.py

--- listCompre.py
+++ listCompre.py
@@ -9,3 +9,12 @@ if __name__ == '__main__':
                               if i + j + k != n ]
 
 print(coordinates)
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    max_value= max(arr)
+    arr = [i for i in arr if i!=max_value]
+    print(max(arr))
